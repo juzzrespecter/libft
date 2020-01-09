ef5326d97ab5089d11a2d54726f496bcb058f8f9
Daniel Rodriguez pueyo <danrodri@c2r4s1.42madrid.com>
2020-01-09 18:33:24 +0100

    molinillo-approved libft

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: danrodri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 21:11:44 by danrodri          #+#    #+#             */
-/*   Updated: 2019/11/19 18:17:41 by danrodri         ###   ########.fr       */
+/*   Updated: 2019/11/19 18:15:34 by danrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ void	ft_putendl_fd(char *s, int fd)
 	if (s)
 	{
 		ft_putstr_fd(s, fd);
-		write('\n' ,fd);
+		ft_putchar_fd('\n', fd);
 	}
 }
